0508f60b0f145867bbc64bd3b9d37d2a84f6edd4
aversan <aversan@mail.uc.edu>
2021-01-31 21:36:06 -0500

    adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,16 @@
+﻿function getIP {
+    (Get-NetIPAddress).IPv4Address | select-string "192*"
+}
+
+write-host(getIP)
+$IP = getIP
+write-host("This machine's IP is $IP")
+
+
+
+#$Hello = "Hello, powershell!"
+#write-Host($Hello)
+
+#Get-Service |select-object * |Out-GridView
+#Get-Service | Sort-Object -Property Status -Descending | format-table DisplayName, Status | Out-File C:\temp\services.txt
+
