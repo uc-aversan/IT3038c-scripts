cc25a88a30f414ed1b88a053b883398af0de3e2b
aversan <aversan@mail.uc.edu>
2021-03-07 23:13:23 -0500

    windowsupdate

--- powershell/windowsupdate.psm1
+++ powershell/windowsupdate.psm1
@@ -1,5 +1,6 @@
 function updateWindows {
-    #Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
     Get-WUList
-
+    #Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+    #C:\IT3038c-scripts\powershell> Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot
+    #C:\IT3038c-scripts\powershell> Remove-WindowsUpdate -KBArticleID KB0000000 -NoRestart
 }
